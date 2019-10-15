0670cff529b2f8f3a5347884e15c624cfc1cb1b5
z3st <pom@localhost.localdomain>
2019-10-15 19:45:14 +0200

    pipe6.c started working with the two first processes
    I think I need to buff and flush the pipeline each time

--- pipe6.c
+++ pipe6.c
@@ -5,6 +5,13 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include "libft/libft.h"
+
+#define	RDBUFF_SIZE 4092
+
+void	err_handler(char *err_msg);
+void	execute_cmd(char *path, char **args, int p[2]);
+
+//======================================================
 //pipelining an arbitrary number of processes together.
 //it implies that the fork/execute part needs to be in 
 //a separate function to be called an arbitrary number
@@ -12,15 +19,14 @@
 //The processes are recieved as arguments to the program,
 //each strings representing the program path and it's
 //arguments separated by whitespaces.
-void	err_handler(char *err_msg);
-void	execute_cmd(char *path, char **args, char **env);
-//!\	by now this is a dirty implementation without input or error checks.
-//!\	The goal is just to practice pipelining.
+//------------------------------------------------------
+//!\ dirty implementations without input/error checking
+//!\ goal is just to learn pipelining.
+//------------------------------------------------------
+//======================================================
 int	main(int argc, char **argv)
 {
-	extern char **environ;
 	//we first get all the commands and separate them into arguments
-	//everything is stocked in cmd_list
 	char ***cmd_list;
 	cmd_list = malloc(sizeof(cmd_list) * argc + 1);
 	int i = 1;
@@ -28,25 +34,37 @@ int	main(int argc, char **argv)
 		cmd_list[i - 1] = ft_strsplit(argv[i], ' ');
 		i++;
 	}
-	//executing the programs one by one (no pipes yet)
-	i = 0;
+	//creating our pipe
+	int p[2];
+	if (pipe(p) < 0)
+		err_handler("pipe error: ");
+	//executing the programs one by one and pipelining them
+	i = -0;
 	while (i < argc - 1) {
-		execute_cmd(cmd_list[i][0], cmd_list[i], environ);
+		execute_cmd(cmd_list[i][0], cmd_list[i], p);
 		i++;
 	}
+	//printing the out-end of the pipe
+	char rd_buff[4092];
+	while (read(p[0], rd_buff, RDBUFF_SIZE) > 0)
+		printf("%s", rd_buff);
 	return 0;
 }
 
-void	execute_cmd(char *path, char **args, char **env)
+void	execute_cmd(char *bin, char **args, int p[2])
 {
 	pid_t child_pid;
-
 	if ((child_pid = fork()) < 0)
 		err_handler("fork error: ");
 	if (child_pid) {
+		close(p[1]);
 		waitpid(child_pid, NULL, 0);
 	} else {
-		execve(path, args, env);
+		dup2(p[0], STDIN_FILENO);
+		dup2(p[1], STDOUT_FILENO);
+		close(p[0]);
+		close(p[1]);
+		execvp(bin, args);
 		perror("exec error: ");
 		exit(EXIT_FAILURE);
 	}
