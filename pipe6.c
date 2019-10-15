05985f8a8f013f19c2907143913f921178a74a46
z3st <pom@localhost.localdomain>
2019-10-15 22:14:48 +0200

    .

--- pipe6.c
+++ pipe6.c
@@ -6,7 +6,7 @@
 #include <sys/wait.h>
 #include "libft/libft.h"
 
-#define	RDBUFF_SIZE 4092
+#define	RDBUFF_SIZE 2
 
 void	err_handler(char *err_msg);
 void	execute_cmd(char *path, char **args, int p[2]);
@@ -35,19 +35,32 @@ int	main(int argc, char **argv)
 		i++;
 	}
 	//creating our pipe
-	int p[2];
-	if (pipe(p) < 0)
-		err_handler("pipe error: ");
 	//executing the programs one by one and pipelining them
-	i = -0;
+	i = 0;
+	char *f_out = ft_strdup("");
 	while (i < argc - 1) {
+		//creating a new pipe
+		int p[2];
+		if (pipe(p) < 0)
+			err_handler("pipe error: ");
+		//writing the output of the last program (or "" if first iteration) into the pipe-in
+		write(p[1], f_out, ft_strlen(f_out));
+		//executing the command, sending it the pipe's fds
 		execute_cmd(cmd_list[i][0], cmd_list[i], p);
+		//reading the command output into tmp
+		char rd_buff[RDBUFF_SIZE] = {0};
+		char *tmp = ft_strdup("");
+		while (read(p[0], rd_buff, RDBUFF_SIZE) > 0)
+			tmp = ft_strjoin(tmp, rd_buff);
+		//closing the pipe
+		close(p[0]);
+		close(p[1]);
+		//copying tmp into f_out
+		f_out = ft_strdup(tmp);
 		i++;
 	}
-	//printing the out-end of the pipe
-	char rd_buff[4092];
-	while (read(p[0], rd_buff, RDBUFF_SIZE) > 0)
-		printf("%s", rd_buff);
+	if (f_out[0])
+		printf("%s", f_out);
 	return 0;
 }
 
@@ -58,7 +71,6 @@ void	execute_cmd(char *bin, char **args, int p[2])
 		err_handler("fork error: ");
 	if (child_pid) {
 		close(p[1]);
-		waitpid(child_pid, NULL, 0);
 	} else {
 		dup2(p[0], STDIN_FILENO);
 		dup2(p[1], STDOUT_FILENO);
